fcc5ce553306cb5158c6784a34add50137bcd3fe
PeteAniDev <btrn2711@gmail.com>
2025-05-28 16:20:49 +0700

    Base controllers

--- JavaCode/src/main/java/main/controller/MainController.java
+++ JavaCode/src/main/java/main/controller/MainController.java
@@ -6,6 +6,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+	@RequestMapping("/")
+	public String home() {
+		return "trang1";
+	}
+
+	@RequestMapping("/Login")
+	public String login() {
+		return "manHinhDangNhapDangKi";
+	}
+
+	@RequestMapping("/Register")
+	public String register() {
+		return "trangThemTaiKhoan";
+	}
+
 	@RequestMapping("/Profiles")
 	public String mapProfiles() {
 		return "manHinhQuanLiTaiKhoan";
